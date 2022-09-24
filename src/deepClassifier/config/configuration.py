f0f95c421442285fac252838bd7b82d595ddc4f1
vtech20 <vtech20@gmail.com>
2022-09-24 13:11:55 +0530

    stage 04 scripts

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -5,7 +5,8 @@ from deepClassifier.entity import (
     DataIngestionConfig, 
     PrepareBaseModelConfig, 
     PrepareCallbacksConfig,
-    TrainingConfig
+    TrainingConfig,
+    EvaluationConfig
 )
 from pathlib import Path
 import os
@@ -86,3 +87,12 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=self.config.training.trained_model_path,
+            training_data=self.config.data_ingestion.unzip_dir,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
